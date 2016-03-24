390b93b77b26b2b3961a184ff1c54ef8a0a84c0d
Tomi Leppänen <tomi.leppanen@telemail.fi>
2016-03-24 19:42:30 +0200

    Changed leds to use charlieplexing
    Leds use now charlieplexing which frees two pins for other use. That way
    we can later add support for another button and a piezo speaker.
    Technically it's possible to add one more led there if wanted.
    
    Removed half minute display because it would be quite a pain to make
    now. It might be added back later.
    
    This implementation sets the timer to about 250 Hz using a trick. The
    trick is to change the TOP value slightly so cycles are a always bit off
    but their average is correct.
    
    Leds are set by tables that define which pins are outputs and which
    pins are high for each led.
    
    After the timer runs out the MCU is kept in sleep mode as much as
    possible (which is not a lot more really).

--- ajastin.c
+++ ajastin.c
@@ -22,11 +22,17 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *
- * This code is made for AtTiny85, but should work with AtTiny45 too.
+ * This is a small timer program for AtTiny85
+ *
+ * This code is made for AtTiny85, but should work with AtTiny[24]5 too.
  * Use 1 MHz clock with this code. It is quite safe to disable brown-out
  * detection as this code doesn't write any memory. The code runs
- * ISR(TIMER0_COMPA_vect) at about 10 Hz. Time is adjusted by TIME in
- * seconds.
+ * ISR(TIMER0_COMPA_vect) (FREQUENCY * NUMBER_OF_LEDS) times a second.
+ * Time is adjusted by TIME in seconds.
+ *
+ * The code uses charlieplexing to drive 5 leds. Therefore every led
+ * will be lit FREQUENCY times per second and their duty cycle is
+ * 1/NUMBER_OF_LEDS.
  */
 
 /* vim: set tw=72 */
@@ -35,71 +41,87 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <avr/sleep.h>
+#include <stdint.h>
 #include <util/delay.h>
 
 // Register definitions
-#define PORTB_VALUE 0x00
-#define DDRB_VALUE 0x1F
 #define TCCR0A_VALUE (1 << WGM01)
-#define TCCR0B_VALUE ((1 << CS02) | (1 << CS00))
-#define OCR0A_VALUE 96;
+#define TCCR0B_VALUE ((1 << CS01) | (1 << CS00))
+#define OCR0A_VALUE 61;
 #define TIMSK_VALUE (1 << OCIE0A)
-
-// Pin definition(s)
-#define LED_ON (1 << PB4)
 
 // Other
 #define TIME 60
-#define MULTIPLIER 10
-#define MAXIMUM 15
+#define FREQUENCY 50
+#define MAXIMUM 0x0F
+#define NUMBER_OF_LEDS 5
+#define LED_ON (1 << 4)
 
-volatile unsigned char counter;
-volatile unsigned int time;
-volatile unsigned char half;
+volatile const unsigned char LEDS_HIGH[] = { // PORTB values
+    0b01000,
+    0b00010,
+    0b10000,
+    0b00010,
+    0b01000
+};
+
+volatile const unsigned char LEDS_INPUT[] = { // DDRB values
+    0b01010,
+    0b10010,
+    0b10010,
+    0b01010,
+    0b11000
+};
+
+volatile uint8_t counter;
+volatile uint8_t leds;
 
 ISR(TIMER0_COMPA_vect)
 {
-    if (++time > TIME * MULTIPLIER) {
-        counter++;
-        time = 0;
-        half = 0;
-    } else if (time > TIME/2 * MULTIPLIER) {
-        half = 1;
+    static uint8_t multiplier = 0;
+    static uint8_t time = 0;
+    uint8_t state = (multiplier % NUMBER_OF_LEDS);
+    PORTB = 0;
+    if (leds & (1 << state)) {
+        DDRB = LEDS_INPUT[state];
+        PORTB = LEDS_HIGH[state];
     }
+    if (multiplier >= FREQUENCY*NUMBER_OF_LEDS) {
+        if (counter < MAXIMUM) {
+            if (++time >= TIME) {
+                counter++;
+                time = 0;
+            }
+        }
+        multiplier = 0;
+    } else {
+        ++multiplier;
+    }
+    OCR0A ^= 2; // Switch between values 61 and 62
 }
 
 int main(void)
 {
-    unsigned char toggle = 0;
-    { // Setup
+    { // Start the timer
         cli();
         set_sleep_mode(SLEEP_MODE_IDLE);
-        PORTB = PORTB_VALUE;
-        DDRB = DDRB_VALUE;
         TCCR0A = TCCR0A_VALUE;
         TCCR0B = TCCR0B_VALUE;
         OCR0A = OCR0A_VALUE;
         TIMSK = TIMSK_VALUE;
         counter = 0;
-        time = 0;
-        half = 0;
-        PORTB = LED_ON;
+        leds = LED_ON;
         sei();
     }
-    // Display
-    while (counter < MAXIMUM)
-    {
-        if (half) {
-            if (toggle)
-                PORTB = LED_ON | (counter+1);
-            else
-                PORTB = LED_ON | counter;
-            toggle = !toggle;
-        } else {
-            PORTB = LED_ON | counter;
+    {// Display the timer while it's running
+        while (counter < MAXIMUM)
+        {
+            leds = counter | LED_ON;
+            sleep_mode();
         }
-        sleep_mode();
+        leds = counter;
     }
-    PORTB &= ~LED_ON;
+    while (1) // Timer finished, sleep forever!
+        sleep_mode();
     return 0;
 }
